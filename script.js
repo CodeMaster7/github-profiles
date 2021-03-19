424a0742471ebf651960a0af3ba8e8566e2b840f
sam <sammybonfanti@gmail.com>
2021-03-18 21:46:14 -0600

    working on the error card

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
 'use strict'
+// GET /users/{ username } ---- https://docs.github.com/en/rest/reference/users#get-a-user
+// GET /users/{ username }/repos ----- https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user
+const API_URL = 'https://api.github.com/users/'
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+async function getUser(username) {
+	try {
+		const { data } = await axios(API_URL + username)
+
+		createUserCard(data)
+	} catch (err) {
+        console.log(err, 'error')
+		if (err.response.status == 404) {
+			createErrorCard('No profile with this username')
+		}
+	}
+}
+
+function createUserCard (user) {
+    const card_HTML = `
+        <div class="card">
+                <div>
+                    <img src="${user.avatar_url}" class="avatar" alt="${user.name}">
+                </div>
+                <div class="user-info">
+                    <h2>${user.name}</h2>
+                    <p>${user.bio}</p>
+
+                    <ul>
+                        <li>${user.followers} <strong>Followers</strong></li>
+                        <li>${user.following} <strong>Following</strong></li>
+                        <li>${user.public_repos} <strong>Repos</strong></li>
+                    </ul>
+
+                    <div id="repos">
+                        <a href="#" class="repo">Repo 1</a>
+                        <a href="#" class="repo">Repo 2</a>
+                        <a href="#" class="repo">Repo 3</a>
+                    </div>
+                </div>
+            </div>
+    `
+    main.innerHTML = card_HTML
+}
+
+function createErrorCard (message) {
+    console.log('hello')
+    const error_card_HTML = `
+        <div class="card">
+            <h1>${message}</h1>
+        </div>
+    `
+
+    main.innerHTML = error_card_HTML
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if (user) {
+        getUser(user)
+        search.value = ''
+    }
+})
